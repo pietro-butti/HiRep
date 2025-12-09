0dbd3b87650964088efe227959cd84126959bec2
Pietro Butti <pietro.butti.fl@gmail.com>
2025-12-09 11:43:40 +0100

    Gaussian + HYP smearing bug solved

--- Disconnected/compute_loops_smeared.c
+++ Disconnected/compute_loops_smeared.c
@@ -140,7 +140,8 @@ int main(int argc, char *argv[]) {
 
         lprintf("MAIN", 0, "Configuration from %s\n", cnfg_filename);
 
-        read_gauge_field(cnfg_filename);
+        unit_u(u_gauge);
+        // read_gauge_field(cnfg_filename);
         // copy_suNg_field(HYP,u_gauge);
 
         represent_gauge_field();
@@ -148,14 +149,9 @@ int main(int argc, char *argv[]) {
         lprintf("TEST", 0, "<p> %1.6f\n", avr_plaquette());
         full_plaquette();
 
-        // HYP_smearing(HYP, u_gauge, HYP_var.weight);
-
-        lprintf("TEST", 0, "<p> %1.6f\n", avr_plaquette());
-        full_plaquette();
-
         lprintf("CORR", 0, "Number of noise vector : nhits = %i \n", disc_var.nhits);
-        lprintf("CORR", 0, "Number of Gaussian smearing levels = %d\n", 0, disc_var.n_smr);
-        lprintf("CORR", 0, "Smearing parameter alpha = %d\n", 0, disc_var.alpha);
+        lprintf("CORR", 0, "Number of Gaussian smearing levels = %d\n", disc_var.n_smr);
+        lprintf("CORR", 0, "Smearing parameter alpha = %f\n", disc_var.alpha);
         measure_loops_smeared(m, disc_var.nhits, i, disc_var.precision, disc_var.source_type, disc_var.n_mom, disc_var.n_smr,
                               disc_var.alpha, HYP_var.weight, DONTSTORE, NULL);
 
--- LibHR/Observables/loop_tools.c
+++ LibHR/Observables/loop_tools.c
@@ -27,96 +27,6 @@
 #error This code does not work with the fermion twisting !!!
 #endif
 
-void _gaussian_smearing(spinor_field *restrict out, spinor_field *restrict in, suNf_field *HYP_f, double alpha) {
-#ifdef CHECK_SPINOR_MATCHING
-    error((in == NULL) || (out == NULL), 1, "Dphi_cpu_ [Dphi.c]", "Attempt to access unallocated memory space");
-    error(in == out, 1, "Dphi_cpu_ [Dphi.c]", "Input and output fields must be different");
-    error(out->type == &glat_even && in->type == &glat_even, 1, "Dphi_cpu_ [Dphi.c]", "Spinors don't match! (1)");
-    error(out->type == &glat_odd && in->type == &glat_odd, 1, "Dphi_cpu_ [Dphi.c]", "Spinors don't match! (2)");
-#endif
-
-    /************************ loop over all lattice sites *************************/
-    /* start communication of input spinor field */
-    start_sendrecv_spinor_field(in);
-
-    _PIECE_FOR(out->type, ixp) {
-#ifdef WITH_MPI
-        if (ixp == out->type->inner_master_pieces) {
-            /* wait for spinor to be transfered */
-            complete_sendrecv_spinor_field(in);
-        }
-#endif
-
-        _SITE_FOR(out->type, ixp, ix) {
-#if defined(_OPENMP) && defined(WITH_PROBE_MPI)
-            register int thread0 = hr_threadId();
-#endif
-
-            int i_dw, i_up;
-            double factor = 1. / (1. + 6. * alpha);
-            suNf *u, *u_dw;
-            suNf_spinor *psi_up, *psi_dw, *r, *rold;
-            suNf_vector chi0, chi1, chi2, chi3;
-
-            r = _FIELD_AT(out, ix);
-            rold = _FIELD_AT(in, ix);
-
-            _spinor_zero_f(*r);
-
-            for (int idir = 1; idir < 4; ++idir) {
-                i_dw = idn(ix, idir);
-                i_up = iup(ix, idir);
-
-                // // This is the issue: pu_gauge_f  is processed as
-                // // pu_gauge_f(ix, mu) ((u_gauge_f->ptr) + coord_to_index(ix, mu))
-                // // but what is u_gauge_f? How can I produce the same from HYP?
-                // u = pu_gauge_f(ix, idir);
-                // u_dw = pu_gauge_f(i_dw, idir);
-
-                u = (HYP_f->ptr) + coord_to_index(ix, idir);
-                u_dw = (HYP_f->ptr) + coord_to_index(i_dw, idir);
-
-                psi_up = _FIELD_AT(in, i_up);
-                psi_dw = _FIELD_AT(in, i_dw);
-
-                // up ------------------------------------------
-                // chi_i <= U_ij(n) * psi_j(n+dir)
-                _suNf_multiply(chi0, *(u), (*psi_up).c[0]);
-                _suNf_multiply(chi1, *(u), (*psi_up).c[1]);
-                _suNf_multiply(chi2, *(u), (*psi_up).c[2]);
-                _suNf_multiply(chi3, *(u), (*psi_up).c[3]);
-
-                // r <= r + chi_i
-                _vector_add_assign_f((*r).c[0], chi0);
-                _vector_add_assign_f((*r).c[1], chi1);
-                _vector_add_assign_f((*r).c[2], chi2);
-                _vector_add_assign_f((*r).c[3], chi3);
-
-                // dw ------------------------------------------
-                // chi_i <= U_ij^dag(n-dir) * psi_j(n-dir)
-                _suNf_inverse_multiply(chi0, *(u_dw), (*psi_dw).c[0]);
-                _suNf_inverse_multiply(chi1, *(u_dw), (*psi_dw).c[1]);
-                _suNf_inverse_multiply(chi2, *(u_dw), (*psi_dw).c[2]);
-                _suNf_inverse_multiply(chi3, *(u_dw), (*psi_dw).c[3]);
-
-                // r <= r + chi_i
-                _vector_add_assign_f((*r).c[0], chi0);
-                _vector_add_assign_f((*r).c[1], chi1);
-                _vector_add_assign_f((*r).c[2], chi2);
-                _vector_add_assign_f((*r).c[3], chi3);
-            }
-
-            /******************************** end of loop *********************************/
-            _spinor_lc_f(*r, factor, *rold, alpha * factor, *r);
-
-#ifdef WITH_PROBE_MPI
-            if (thread0 == 0) { probe_mpi(); }
-#endif
-
-        } /* SITE_FOR */
-    } /* PIECE FOR */
-}
-
 void represent_smeared_field(suNg_field *HYP, suNf_field *HYP_f) {
     /* loop on local lattice first */
     /* loop on the rest of master sites */
@@ -579,7 +489,7 @@ void measure_loops_smeared(double *m, int nhits, int conf_num, double precision,
     if (source_type == 4) { lprintf("CORR", 0, "Time, spin , color and eo dilution  will be used \n"); }
     if (source_type == 5) { lprintf("CORR", 0, "Spin , color and eo dilution  will be used \n"); }
     if (source_type == 6) {
-        lprintf("CORR", 0, "Time and spin dilution with Gaussian smearing + HYP smearing will be used \n\n");
+        lprintf("CORR", 0, "Time and spin dilution with Gaussian smearing + HYP smearing will be used \n");
     }
 
     gettimeofday(&start, 0);
@@ -784,10 +694,13 @@ void measure_loops_smeared(double *m, int nhits, int conf_num, double precision,
 
                 for (int ismr = 0; ismr < n_smr; ismr++) {
                     for (int beta = 0; beta < 4; beta++) {
-                        _gaussian_smearing(&source1[beta], &source[beta], HYP_f, alpha);
+                        gaussian_smearing(&source1[beta], &source[beta], HYP_f, alpha);
                     }
-                    source = source1;
+                    for (int beta = 0; beta < 4; beta++) {
+                        gaussian_smearing(&source[beta], &source1[beta], HYP_f, alpha);
+                    }
                 }
+
                 calc_propagator(prop, source, 4); //4 for spin dilution
 #ifdef WITH_GPU
                 for (int beta = 0; beta < 4; beta++) {
